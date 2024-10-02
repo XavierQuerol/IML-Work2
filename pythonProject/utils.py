836348b4224009d7d1fd409dd0a47d77c9ccdb31
XavierQuerol <101579003+XavierQuerol@users.noreply.github.com>
2024-10-02 19:49:08 +0200

    xavi functions

--- pythonProject/utils.py
+++ pythonProject/utils.py
@@ -1,9 +1,14 @@
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.linear_model import LinearRegression
 
 
 def drop_columns(df, column_names):
-    # xavier
-    pass
+    df = df.remove(column_names = column_names)
+    return df
+
+def drop_rows(df, column_names):
+    df = df.dropna(subset=[column_names])
+    return df
 
 def min_max_scaler(df_train, df_test, column_names):
     ## berni
@@ -17,6 +22,15 @@ def binary_encoding(df):
     ## angelica
     pass
 
-def drop_nans(df):
-    ## xavier
-    pass
+def fill_nans(df_train, df_test, columns_train, columns_predict):
+    model = LinearRegression()
+
+    x = df_train[columns_train]
+    y = df_train[columns_predict]
+
+    model.fit(x, y)
+
+    df_train[columns_predict] = model.predict(x)
+    df_test[columns_predict] = model.predict(df_test[columns_train])
+
+    return df_train, df_test
