31619f8a15bbca01fb7eb3ef5ce529d941cf2d95
XavierQuerol <101579003+XavierQuerol@users.noreply.github.com>
2024-10-02 18:11:44 +0200

    upload files and first .ipynb

---
+++ pythonProject/utils.py
@@ -0,0 +1,22 @@
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+
+
+def drop_columns(df, column_names):
+    # xavier
+    pass
+
+def min_max_scaler(df_train, df_test, column_names):
+    ## berni
+    return df_train, df_test
+
+def one_hot_encoding(df):
+    ## aina
+    pass
+
+def binary_encoding(df):
+    ## angelica
+    pass
+
+def drop_nans(df):
+    ## xavier
+    pass
