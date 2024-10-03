9b6a17ab56957eb77f0a3771012729015d423211
XavierQuerol <101579003+XavierQuerol@users.noreply.github.com>
2024-10-03 23:58:24 +0200

    fill_nans fixed

--- pythonProject/utils.py
+++ pythonProject/utils.py
@@ -1,5 +1,6 @@
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn.linear_model import LinearRegression
+import pandas as pd
 
 
 def drop_columns(df, column_names):
@@ -66,12 +67,12 @@ def binary_encoding(df_train, df_test):
 def fill_nans(df_train, df_test, columns_train, columns_predict):
     model = LinearRegression()
 
-    x = df_train[columns_train]
-    y = df_train[columns_predict]
+    x = pd.concat((df_train[columns_train], df_test[columns_train]))
+    y = pd.concat((df_train[columns_predict], df_test[columns_predict]))
 
     model.fit(x, y)
 
-    df_train[columns_predict] = model.predict(x)
+    df_train[columns_predict] = model.predict(df_train[columns_train])
     df_test[columns_predict] = model.predict(df_test[columns_train])
 
     return df_train, df_test
